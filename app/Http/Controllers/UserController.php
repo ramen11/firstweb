84dc0f45bd23936fa342b91e1f35f44415b6127d
ramen11 <kikjunsiang@gmail.com>
2025-11-25 10:04:45 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $incomingFields = $request->validate([
+            'name' =>['required','min:3','max:10'],
+            'email' => ['required','email'],
+            'password' => ['required','miin:8','max:200']
+        ]);
+        return'bonjour from our controller';
+    }
+}
